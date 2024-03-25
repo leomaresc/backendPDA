55e3e85a60f4447e084093c34b0083389837f354
leomaresc <leomarsalazarec@gmail.com>
2024-03-25 15:19:51 -0300

    Cambiado puerto por puerto universal.

--- src/api/server.js
+++ src/api/server.js
@@ -6,7 +6,7 @@ import LoginRegister from './routes/LoginRegister.js'
 const app = express();
 // Para que funcionen las pruebas en expo se debe cocolor aquí de manera manual la ipv4 de la maquina donde se está ejecutando el servidor.
 const hostname = "localhost";
-const port = 3000;
+const port = 80;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
